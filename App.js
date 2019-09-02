6530672911b3f96117e3ee756a6c4aa742982697
Senthuran Elangovan <senthuran@pikanite.com>
2019-09-02 12:04:28 +0530

    App file  modified

--- App.js
+++ App.js
@@ -1,41 +1,10 @@
-import React , {Component} from 'react';
-import {
-  StyleSheet,
-  Text,
-  TextInput,
-  View
-} from 'react-native';
+import React, {
+  Component
+} from 'react';
+import Main from './src/Main';
 
 export default class App extends Component {
-  // state = {
-  //   text : "Sen2"
-  // }
-  render(){
-    return (
-      <View style={{flex: 1}}>
-         <View style={{flex: 1, backgroundColor: 'powderblue'}} >
-           <Text>Navigation</Text>
-         </View>
-         <View style={{flex: 5, backgroundColor: 'skyblue'}} />
-         <View style={{flex: 1, backgroundColor: 'steelblue'}} >
-           <Text>Footer</Text>
-             {/* <TextInput
-               style={{height: 40}}
-               placeholder="Type here to translate!"
-               onChangeText={(text) => this.setState({text})}
-               value={this.state.text}
-             /> */}
-         </View>
-       </View> 
-     );
+  render() {
+    return <Main />;
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
