8fb2f5e494de1c8a018fa0ea54a8de33da12536b
Senthuran Elangovan <senthuran@pikanite.com>
2019-09-02 16:19:28 +0530

    input component added

--- src/Main.js
+++ src/Main.js
@@ -4,21 +4,48 @@ import React, {
 import {
     StyleSheet,
     Text,
+    TextInput,
     View,
     StatusBar
 } from 'react-native'
 import Header from './components/Header/Header';
+import Input from './components/TextInput/TextInput';
+// import {
+//     inputPlaceholder
+// } from '../src/utils/Colors'
+
 
 const headerTitle = "TO DO"
 
 export default class Main extends Component {
+    state = {
+        inputValue : ""
+    }
+
+    newInputValue = value =>{
+        this.setState({
+            inputValue : value
+        })
+    }
+
     
     render(){
+
+        const {
+            inputValue
+        } = this.state;
+
         return(
             <View  style={styles.container}>
                 <StatusBar barStyle="light-content" />
                 <View style={styles.centered}>
                     <Header title={headerTitle} />
+                </View>
+                <View style={styles.inputContainer}>
+                    <Input 
+                        inputValue={inputValue} 
+                        onChangeText={this.newInputValue} 
+                    />
                 </View>
             </View>
         )
@@ -28,9 +55,16 @@ export default class Main extends Component {
 const styles = StyleSheet.create({
     container : {
         flex : 1,
-        backgroundColor: 'powderblue'
+        backgroundColor: 'coral'
     },
     centered :{
+        // flex : 1,
+        backgroundColor: '#CC6640',
         alignItems : "center"
-    }
+    },
+    inputContainer :{
+        // flex : 5,
+        marginTop : 40,
+        paddingLeft : 15
+    },
 })
